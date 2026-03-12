46b7ce5f7d7db84945d324a0f21010112f282261
Bruno Pinto <1250652@isep.ipp.pt>
2026-03-12 16:52:08 +0000

    km/h
    foi colocado no ecrã a velocidade que estamos no carro, mas em km por hora

---
+++ resources/[standalone]/simple_speedometer/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Boss Engineering'
+description 'Velocímetro Simples em KM/H'
+version '1.0.0'
+
+shared_scripts { '@qb-core/shared/locale.lua' }
+
+client_scripts {
+    'client.lua'
+}
+
+lua54 'yes'
